0154c68f0539896ca8c52f89ef172fb302c5bf7d
Duy Linh <22520762@gm.uit.edu.vn>
2024-12-03 14:03:25 +0700

    Rsa decimal

---
+++ DoAnN/RSAForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DoAnN
+{
+    partial class RSAForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPublicKey = new System.Windows.Forms.TextBox();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.btnGenerateKeys = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPublicKey
+            // 
+            this.txtPublicKey.Location = new System.Drawing.Point(68, 33);
+            this.txtPublicKey.Name = "txtPublicKey";
+            this.txtPublicKey.Size = new System.Drawing.Size(465, 22);
+            this.txtPublicKey.TabIndex = 0;
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Location = new System.Drawing.Point(68, 74);
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.Size = new System.Drawing.Size(465, 22);
+            this.txtPrivateKey.TabIndex = 1;
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(298, 114);
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(467, 22);
+            this.txtPlaintext.TabIndex = 1;
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Location = new System.Drawing.Point(298, 200);
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(467, 22);
+            this.txtCiphertext.TabIndex = 1;
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(298, 313);
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(467, 22);
+            this.txtDecrypted.TabIndex = 2;
+            // 
+            // btnGenerateKeys
+            // 
+            this.btnGenerateKeys.Location = new System.Drawing.Point(191, 171);
+            this.btnGenerateKeys.Name = "btnGenerateKeys";
+            this.btnGenerateKeys.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerateKeys.TabIndex = 3;
+            this.btnGenerateKeys.Text = "btnGenerateKeys";
+            this.btnGenerateKeys.UseVisualStyleBackColor = true;
+            this.btnGenerateKeys.Click += new System.EventHandler(this.btnGenerateKeys_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(374, 171);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "btnEncrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(374, 267);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "btnDecrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // RSAForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnGenerateKeys);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.txtCiphertext);
+            this.Controls.Add(this.txtPlaintext);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.txtPublicKey);
+            this.Name = "RSAForm";
+            this.Text = "RSAForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPublicKey;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Button btnGenerateKeys;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
---
+++ DoAnN/RSAForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DoAnN
+{
+    public partial class RSAForm : Form
+    {
+        private BigInteger p, q, n, phi, ee, d;
+
+        public RSAForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsPrime(BigInteger n)
+        {
+            if (n < 2) return false;
+            for (BigInteger i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        private BigInteger Gcd(BigInteger a, BigInteger b)
+        {
+            while (b != 0)
+            {
+                BigInteger temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private BigInteger ModInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m, x0 = 0, x1 = 1;
+
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                BigInteger q = a / m;
+                BigInteger t = m;
+
+                m = a % m;
+                a = t;
+                t = x0;
+
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            return x1 < 0 ? x1 + m0 : x1;
+        }
+
+        private void GenerateKeyPair()
+        {
+            // Tạo số nguyên tố p và q
+            Random rnd = new Random();
+            do
+            {
+
+
+                p = GeneratePrime(rnd);
+
+                q = GeneratePrime(rnd);
+            }
+            while (p==q);
+
+            n = p * q;
+            phi = (p - 1) * (q - 1);
+
+            // Chọn e sao cho gcd(e, phi) = 1
+            do
+            {
+                ee = rnd.Next(2, (int)phi);
+            } while (Gcd(ee, phi) != 1);
+
+            // Tính d = e^(-1) mod phi
+            d = ModInverse(ee, phi);
+        }
+
+        private BigInteger GeneratePrime(Random rnd)
+        {
+            while (true)
+            {
+                BigInteger candidate = rnd.Next(1000, 5000); // Tùy chỉnh dải số nguyên tố
+                if (IsPrime(candidate))
+                    return candidate;
+            }
+        }
+
+        private string Encrypt(string plaintext)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(plaintext);
+            BigInteger[] cipherBytes = new BigInteger[bytes.Length];
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                cipherBytes[i] = BigInteger.ModPow(bytes[i], ee, n);
+            }
+
+            return string.Join(" ", cipherBytes);
+        }
+
+        private string Decrypt(string ciphertext)
+        {
+            string[] parts = ciphertext.Split(' ');
+            byte[] plainBytes = new byte[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                BigInteger cipherValue = BigInteger.Parse(parts[i]);
+                plainBytes[i] = (byte)BigInteger.ModPow(cipherValue, d, n);
+            }
+
+            return Encoding.UTF8.GetString(plainBytes);
+        }
+
+        private void btnGenerateKeys_Click(object sender, EventArgs e)
+        {
+            GenerateKeyPair();
+            txtPublicKey.Text = $"e: {ee}, n: {n}, p: {p}, q: {q};";
+            txtPrivateKey.Text = $"d: {d}, n: {n}";
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPlaintext.Text))
+            {
+                MessageBox.Show("Vui lòng nhập thông điệp cần mã hóa.");
+                return;
+            }
+
+            string ciphertext = Encrypt(txtPlaintext.Text);
+            txtCiphertext.Text = ciphertext;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCiphertext.Text))
+            {
+                MessageBox.Show("Vui lòng nhập bản mã cần giải mã.");
+                return;
+            }
+
+            string plaintext = Decrypt(txtCiphertext.Text);
+            txtDecrypted.Text = plaintext;
+        }
+    }
+}
